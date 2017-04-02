02232b70396547cde75f6e74b3addaa96026500e
Ashwini <ashie.oh@gmail.com>
2017-04-02 18:12:59 +0100

    Add a new view for displaying single news article summary

---
+++ public/js/view/SingleNewsView.js
@@ -0,0 +1,14 @@
+(function(exports){
+  'use strict';
+
+  function SingleNewsView(news) {
+    this.news = news
+  }
+
+  SingleNewsView.prototype.summaryHTML = function () {
+    return "<img src='" + this.news.getImage() + "'>"+
+    "<br><a href='" + this.news.getWebUrl() +"'>Click here for detailed news :)</a>"+
+    "<p>"+ this.news.getSummary() +"</p>";
+  };
+  exports.SingleNewsView = SingleNewsView;
+})(this);

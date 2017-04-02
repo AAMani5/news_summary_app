2a7f3fa0d38e7c392d0f12213fabb3af38850111
Ashwini <ashie.oh@gmail.com>
2017-04-02 17:57:33 +0100

    Add code to check if summary printed

--- public/js/model/News.js
+++ public/js/model/News.js
@@ -5,12 +5,14 @@
     this.webTitle = newsObject.webTitle;
     this.webUrl = newsObject.webUrl;
     this.image = newsObject.fields.thumbnail;
+    this.summary = newsObject.blocks.body[0].bodyTextSummary;
   }
 
   News.prototype.getSummary = function () {
-    // use api to get summary
+    // use api to get summary & move the printing to seperate view
     return "<img src='" + this.image + "'>"+
-    "<br><a href='" + this.webUrl +"'>Click here for detailed news :)</a>";
+    "<br><a href='" + this.webUrl +"'>Click here for detailed news :)</a>"+
+    "<p>"+ this.summary +"</p>";
   };
   exports.News = News;
 })(this);
